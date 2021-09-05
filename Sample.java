a38de5a700858d33693915b25055ea580e01ddea
SwCodesRi <swastika15.mukherjee@gmail.com>
2021-09-06 01:08:24 +0530

    Committed changes for polling

--- Sample.java
+++ Sample.java
@@ -1,5 +1,5 @@
 class Sample{
     public static void main(String[] args) {
-        System.out.println("Hello World from Durgapur!"); 
+        System.out.println("Hello World from DurgS!"); 
     }
 }
